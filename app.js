c586b7681a56d99f05e7c9edff750ab8e957b070
monmonplaza <monmon.plaza@gmail.com>
2020-02-28 14:54:14 +0800

    Updates

---
+++ app.js
@@ -0,0 +1,61 @@
+let form = document.querySelector("#form");
+let fname = document.querySelector("#fname").value;
+let pos = document.querySelector("#pos").value;
+let email = document.querySelector("#mail").value;
+let inputs = document.querySelectorAll(".inputs");
+
+inputs.forEach(input => {
+  input.addEventListener("change", e => {
+    e.preventDefault();
+    let html = `
+            <table>
+            <tr>
+              <td colspan="2">
+                <h2
+                  style=" font-size: 25px; text-transform: uppercase; font-weight: bolder; padding-left:10px"
+                >
+                 ${document.querySelector("#fname").value}
+                </h2>
+                <h3
+                  style=" font-size: 12px; margin-bottom: 5px; font-weight: 400; color: #787878; font-weight: bold; text-transform:capitalize;padding-left:10px"
+                >
+                ${document.querySelector("#pos").value}
+                </h3>
+              </td>
+              <td></td>
+            </tr>
+            <tr>
+              <td>
+                <img
+                  src="https://frontlinebusiness.com.ph/assets/images/FBS-LOGO-800X200.png"
+                  alt=""
+                  style="display: block; width: 80%"
+                />
+              </td>
+              <td style="border-left: 1px solid #ccc; padding-left: 10px">
+                <a
+                  href="mailto: ${document.querySelector("#mail").value}"
+                  style="display: block; font-size: 14px; margin-bottom: 5px; font-weight: 400; color: #787878; text-decoration: none!important;"
+                  >
+                  ${document.querySelector("#mail").value}</a
+                >
+                <a
+                  href="http://www.frontlinebusiness.com.ph"
+                  target="blank"
+                  style="display: block; font-size: 14px; margin-bottom: 5px; font-weight: 400; color: #787878; text-decoration: none!important; "
+                  >
+                  frontlinebusiness.com.ph</a
+                >
+                <p
+                  style="display: block; font-size:14px; margin-bottom: 5px; font-weight: 400;  text-decoration: none"
+                >
+                   0922 123 1234
+                </p>
+              </td>
+            </tr>
+          </table>
+            `;
+
+    document.querySelector(".signature-box").innerHTML = html;
+  });
+});
